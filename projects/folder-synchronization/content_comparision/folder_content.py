845afeb4f716dff401d3e15c302ced7b8ed6c195
Debora Wendland Amorim <deborawendland@gmail.com>
2023-09-01 14:20:18 +0200

    feat: add folder content methods

---
+++ projects/folder-synchronization/content_comparision/folder_content.py
@@ -0,0 +1,49 @@
+import os
+
+def if_folder_in_path(path, items):
+    for item in items:
+        if os.path.isdir(f"{path}/{item}"):
+            return True
+    return False
+
+def get_folders_in_path(folder):
+    for item in folder["items"]:
+        path_item = "{path}/{item}".format(path=folder["path"])
+        if os.path.isdir(path_item):
+            folder_items = get_folder_content(path_item, False)
+            new_folder = {
+                "path": path_item,
+                "items": folder_items
+            }
+            folder["items"][item] = new_folder
+
+            get_structure(new_folder)
+            
+    return folder
+
+def get_folder_content(path, replica):
+    # print (f"checking items in {path}...")
+    if os.path.isdir(path):
+        # print (f"{path}: {os.listdir(path)}")
+        return os.listdir(path)
+    else:
+        if replica:
+            # print (f"creating {path} directory...")
+            os.mkdir(path)
+            return []
+        else:
+            raise NotADirectoryError("Folder not found.")
+        
+def get_structure(folder):
+    if if_folder_in_path(folder["path"], folder["items"]):
+        for index, item in enumerate(folder["items"]):
+            path_item = "{path}/{item}".format(path=folder["path"], item=item)
+            if os.path.isdir(path_item):
+                folder_items = get_folder_content(path_item, False)
+                new_folder = {
+                    "path": path_item,
+                    "items": folder_items
+                }
+                folder["items"][index] = new_folder
+                get_structure(new_folder)
+    return folder
