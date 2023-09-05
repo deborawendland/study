fdf1216c2335ab3a6986e41a728fd7ba81e7065e
Debora Wendland Amorim <deborawendland@gmail.com>
2023-09-05 13:21:27 +0200

    refactor: simplify content_comparision

--- projects/folder-synchronization/content_comparision/content_comparision.py
+++ projects/folder-synchronization/content_comparision/content_comparision.py
@@ -1,77 +1,75 @@
 import os
 import filecmp
 
-def compare_items_source_to_replica(source, replica, operations, item_source):
-    path_item_source = os.path.normpath(os.path.join(source['path'], item_source))
-    if item_source in replica['items']:
-        path_item_replica = os.path.normpath(os.path.join(replica['path'], item_source))
-        if filecmp.cmp(path_item_source, path_item_replica, shallow=False):
-            operations['items']['keep'].append(path_item_replica)
-        else:
-            operations['items']['copy'].append((path_item_source, path_item_replica))
+def create_path(path, file):
+    return os.path.normpath(os.path.join(path, file))
 
-    else:
-        path_item_replica = os.path.normpath(os.path.join(replica['path'], item_source))
-        operations['items']['create'].append((path_item_source, path_item_replica))
+def register_operation(operation, path, file):
+    filepath = create_path(path, file)
+    operation.append(filepath)
+    return {
+        'path': filepath,
+        'items': []
+    }
 
-def compare_items_replica_to_source(source, replica, operations, item_replica):
-    path_item_replica = os.path.normpath(os.path.join(replica['path'], item_replica))
-    if item_replica not in source['items']:
-        operations['items']['delete'].append(path_item_replica)
-
-def compare_folders_source_to_replica(replica, operations, item_source):
-    if not replica['items']:
-        path_folder_replica = os.path.normpath(os.path.join(replica['path'], os.path.basename(item_source['path'])))
-        operations['folders']['create'].append(path_folder_replica)
-        item_replica = {
-            'path': path_folder_replica,
-            'items': []
-        }
-        compare_structures(item_source, item_replica, operations)
-
-    else: 
-        replica_folders = []
-        for item_replica in replica['items']:
-            if type(item_replica) is dict:
-                replica_folders.append(os.path.basename(item_replica['path']))
-                if os.path.basename(item_source['path']) == os.path.basename(item_replica['path']):
-                    path_folder_replica = os.path.normpath(os.path.join(replica['path'], os.path.basename(item_source['path'])))
-                    operations['folders']['keep'].append(path_folder_replica)
-                    compare_structures(item_source, item_replica, operations)
-        
-        if os.path.basename(item_source['path']) not in replica_folders:
-            path_folder_replica = os.path.normpath(os.path.join(replica['path'], os.path.basename(item_source['path'])))
-            operations['folders']['create'].append(path_folder_replica)
-            item_replica = {
-                'path': path_folder_replica,
-                'items': []
-            }
-            compare_structures(item_source, item_replica, operations)
+def compare_items(source, replica, item, operations, is_source):
+    path_item_source = create_path(source['path'], item)
+    path_item_replica = create_path(replica['path'], item)
     
-def compare_folders_replica_to_source(source, operations, item_replica):
-    source_folders = []
-    for item_source in source['items']:
-        if type(item_source) is dict:
-            source_folders.append(os.path.basename(item_source['path']))
+    if is_source:
+        if item in replica['items']:
+            # compare content of two files, if equal keep, otherwise copy
+            if filecmp.cmp(path_item_source, path_item_replica, shallow=False):
+                operations['items']['keep'].append(path_item_replica)
+            else:
+                operations['items']['copy'].append((path_item_source, path_item_replica))
+        else:
+            operations['items']['create'].append((path_item_source, path_item_replica))
+    else: 
+        if item not in source['items']:
+            operations['items']['delete'].append(path_item_replica)
 
-    if os.path.basename(item_replica['path']) not in source_folders:
-        operations['folders']['delete'].append(item_replica['path'])
-        item_source = {
-            'path': os.path.normpath(os.path.join(source['path'], os.path.basename(item_replica['path']))),
-            'items': []
-        }
-        compare_structures(item_source, item_replica, operations)
+def compare_folders(folder, item, operations, is_source):    
+    if is_source:
+        # if replica items is empty, create item (folder) and so recursively 
+        if not folder['items']:
+            item_replica = register_operation(operations['folders']['create'], folder['path'], os.path.basename(item['path']))
+            compare_structures(item, item_replica, operations)
+
+        else: 
+            replica_folders = []
+            # there is a item (folder) in replica, checking if it also contains other items inside it in replica, if so compare it recursively
+            for item_replica in folder['items']:
+                if type(item_replica) is dict:
+                    replica_folders.append(os.path.basename(item_replica['path']))
+                    if os.path.basename(item['path']) == os.path.basename(item_replica['path']):
+                        register_operation(operations['folders']['keep'], folder['path'], os.path.basename(item['path']))
+                        compare_structures(item, item_replica, operations)
+            
+            # if the item (folder) doesn't exist in replica, create it, and so on recursively
+            if os.path.basename(item['path']) not in replica_folders: 
+                item_replica = register_operation(operations['folders']['create'], folder['path'], os.path.basename(item['path']))
+                compare_structures(item, item_replica, operations)
+    else:
+        # for each folder existing only in replica, it will compare recursively to delete it and its items
+        source_folders = []
+        for item_source in folder['items']:
+            if type(item_source) is dict:
+                source_folders.append(os.path.basename(item_source['path']))
+
+        if os.path.basename(item['path']) not in source_folders:
+            item_source = register_operation(operations['folders']['delete'], folder['path'], os.path.basename(item['path']))
+            compare_structures(item_source, item, operations)
 
 def compare_structures(source, replica, operations):
     for item_source in source['items']:
         if type(item_source) is dict:
-            compare_folders_source_to_replica(replica, operations, item_source)
+            compare_folders(replica, item_source, operations, True)
         else:
-            compare_items_source_to_replica(source, replica, operations, item_source)
+            compare_items(source, replica, item_source, operations, True)
     
     for item_replica in replica['items']:
         if type(item_replica) is dict:
-            compare_folders_replica_to_source(source, operations, item_replica)
+            compare_folders(source, item_replica, operations, False)
         else:
-            compare_items_replica_to_source(source, replica, operations, item_replica)
-
+            compare_items(source, replica, item_replica, operations, False)
