8319638a5a768f49ea41f5329e4e3225a5f82fa5
Debora Wendland Amorim <deborawendland@gmail.com>
2023-09-05 10:41:35 +0200

    feat: add create folder operation

--- projects/folder-synchronization/operations/folder_operations.py
+++ projects/folder-synchronization/operations/folder_operations.py
@@ -4,7 +4,12 @@ import sys
 
 def create_folders(folders):
     for folder in folders:
-        logging.info(f'creating folder:   {folder}...')
+        try: 
+            os.mkdir(folder) 
+            logging.info(f'creating folder:   {folder}...')
+        except OSError as error: 
+            print(error) 
+            logging.error(f'error creating folder:   {folder} - {error}')
 
 def delete_folders(folders):
     for folder in folders:
