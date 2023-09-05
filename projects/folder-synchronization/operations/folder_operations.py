b288a288a7ecb47d7bf825a75a984544bccbf08a
Debora Wendland Amorim <deborawendland@gmail.com>
2023-09-05 10:50:54 +0200

    feat: add delete folder operation

--- projects/folder-synchronization/operations/folder_operations.py
+++ projects/folder-synchronization/operations/folder_operations.py
@@ -6,14 +6,18 @@ def create_folders(folders):
     for folder in folders:
         try: 
             os.mkdir(folder) 
-            logging.info(f'creating folder:   {folder}...')
+            logging.info(f'created folder:   {folder}')
         except OSError as error: 
             print(error) 
             logging.error(f'error creating folder:   {folder} - {error}')
 
 def delete_folders(folders):
     for folder in folders:
-        logging.info(f'deleting folder:   {folder}...')
+        try:
+            os.removedirs(folder)
+            logging.info(f'deleted folder:   {folder}')
+        except OSError as error:
+            logging.error(f'error deleting folder:   {folder} - {error}')
 
 def keep_folders(folders):
     for folder in folders:
