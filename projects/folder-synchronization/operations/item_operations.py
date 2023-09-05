eea9aa575dbcfbc4c9ce5852ec727121d5070850
Debora Wendland Amorim <deborawendland@gmail.com>
2023-09-05 10:59:28 +0200

    feat: create item operation

--- projects/folder-synchronization/operations/item_operations.py
+++ projects/folder-synchronization/operations/item_operations.py
@@ -1,8 +1,14 @@
 import logging
+import os
+import shutil
 
 def create_items(items):
     for source, replica in items:
-        logging.info(f'creating item:     {source} to {replica}...')
+        try:
+            shutil.copyfile(source, replica)
+            logging.info(f'created item:     {source} to {replica}')
+        except Exception as error:
+            logging.error(f'created item:     {source} to {replica} - {error}')
 
 def delete_items(items):
     for item in items:
