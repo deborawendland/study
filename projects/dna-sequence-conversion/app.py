2e5eef2a4f1fcbd0a26e1c2beae2b7f811841be8
Debora Wendland Amorim <deborawendland@gmail.com>
2023-09-27 17:44:16 +0200

    chores: add prints and fix filepath name

--- projects/dna-sequence-conversion/app.py
+++ projects/dna-sequence-conversion/app.py
@@ -1,19 +1,24 @@
 import settings
 import numpy as np
-import encode.binary as binary
+from encode import binary
+import os
 
 def read_input_file():
+    print (f'reading file: {settings.INPUT_FILE}')
     file_bytes = np.fromfile(settings.INPUT_FILE, dtype = "uint8")
     return np.unpackbits(file_bytes)
 
 def write_output_file(output):
-    with open(f'{settings.OUTPUT_FOLDER}/output_{settings.L}', "w") as file:
+    filepath =  os.path.join(settings.OUTPUT_FOLDER, f'output_{settings.L}')
+    print (f'creating output file: {filepath}')
+    with open(filepath, "w") as file:
         file.write(output)
+    print ('...OK')
 
 def main():
 
     file_bits = read_input_file()
-    content_encoded = binary.enconde_file(file_bits)
+    content_encoded = binary.enconde_file(file_bits, settings.L)
     output = binary.build_output(content_encoded)
     write_output_file(output)
 
