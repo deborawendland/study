9071e2c7a78c9d2d96400c3b265a2b255ec19c47
Debora Wendland Amorim <deborawendland@gmail.com>
2023-09-27 15:03:39 +0200

    feat: write output file

--- projects/dna-sequence-conversion/app.py
+++ projects/dna-sequence-conversion/app.py
@@ -1,33 +1,23 @@
 import settings
 import numpy as np
 import encode.binary as binary
-import encode.file as en_file
+import encode.file_output as file_output
 
 def read_input_file():
     file_bytes = np.fromfile(settings.INPUT_FILE, dtype = "uint8")
     return np.unpackbits(file_bytes)
 
 def write_output_file(output):
-    ''
+    with open(f'{settings.OUTPUT_FOLDER}/output_{settings.L}', "w") as file:
+        file.write(output)
 
 def main():
 
     file_bits = read_input_file()
-    per_line = settings.L
- 
-    len_byte = 8
-    result = []
-    for l in range(0, int(len(file_bits)), len_byte*per_line): #linha
-        line = []
-        for i in range(0, len_byte*per_line, len_byte): #byte
-            bits = []
-            for count in range (0, len_byte): #bit
-                bits.append(file_bits[l+i+count])
-            line.append(binary.encode_byte(bits))
-        result.append(line)
-    print (en_file.build_output_file(result))
+    content_encoded = binary.enconde_file(file_bits)
+    output = file_output.build_output(content_encoded)
+    write_output_file(output)
 
-    
 
 if __name__ == '__main__':
     main()
